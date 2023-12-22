b3230eacb5e48b304c633dcc80127faaeec3d14e
Jakob A <jdanders1999@gmail.com>
2023-12-21 19:40:18 -0500

    added local storage

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+var submitButton = document.querySelector("get info");
+
+renderLastRegistered();
+
+function displayMessage(type, message) {
+    msgDiv.textConter = message;
+    msgDiv.setAttribute("class", type);
+}
+
+function renderLastRegistered() {
+    var submitButton = localStorage.getItem("get info");
+
+    if (!submitbutton || ) {
+        return;
+    }
+    submitButton.textContent = submitButton;
+}
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var submitButton = submitButtonInput.value;
+    if (submitButton === "") {
+        displayMessage("Please select a date.");
+
+        localStorage.setItem("date", "weather");
+        renderLastRegistered();
+        }
+});
+
+
