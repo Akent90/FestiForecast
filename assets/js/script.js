f289cc642875d1bb9432ef85db2ac0ce5ecc9421
akent <alexanderkent099@gmail.com>
2023-12-21 19:45:03 -0500

    Add API keys and initial DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const holidayApiKey = 52dd97a0-dca8-4f8a-bded-86b768341680;
+const weatherApiKey = b15a810c1209f985b7d2e24e97487aab;
+
+const inputLocation = document.getElementById('inputLocation');
+const submitButton = document.getElementById('submitButton');
+const holidayContainer = document.getElementById('holidaySection');
+const weatherContainer = document.getElementById('weatherSection');
