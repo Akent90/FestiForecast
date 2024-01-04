4eafd3295dcf4e0bfd19d772aec3ab01e481f1d2
Jakob A <jdanders1999@gmail.com>
2024-01-03 20:38:20 -0500

    added notes to js, event listener, updated css, added links to html for the weather usage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//local storage, event listener, and alert
 var inputValue = document.querySelector("#inputLocation");
 var submitButton = document.querySelector("#submitButton")
 
@@ -15,11 +16,18 @@ submitButton.addEventListener("click", function(event) {
     event.preventDefault();
     
     var input = inputValue.value;
+    var inputValue =JSON.parse(localStorage.getItem("location"));
 
     if (input === "") {
-        alert("Please select a date.")
+        alert("Please select a location.")
 
         localStorage.setItem("submitButton", "date");
         renderLastRegistered();
     }
 });
+//event listener for cards
+weatherSection.addEventListener("click", function(event) {
+    event.preventDefault();
+console.log("hello")
+    var input = weatherSection.value;
+})
